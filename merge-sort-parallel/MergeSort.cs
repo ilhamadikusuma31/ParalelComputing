374be8e84593c2919dd60256cb47d98b4945f333
ilham adikusuma <70935258+ilhamadikusuma31@users.noreply.github.com>
2022-05-11 21:22:21 +0700

    Add files via upload

---
+++ merge-sort-parallel/MergeSort.cs
@@ -0,0 +1,79 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Runtime.InteropServices;
+//using System.Threading.Tasks;
+//using System.Diagnostics;
+
+//namespace ParalelComputing
+//{
+//    class MergeSort
+//    {
+
+//        public int[] arr;
+//        public MergeSort(int[] ARR)
+//        {
+//            this.arr = ARR;
+//        }
+//        static public void MainMerge(int[] numbers, int left, int mid, int right)
+//        {
+//            int[] temp = new int[25];
+//            int i, eol, num, pos;
+//            eol = (mid - 1);
+//            pos = left;
+//            num = (right - left + 1);
+
+//            while ((left <= eol) && (mid <= right))
+//            {
+//                if (numbers[left] <= numbers[mid])
+//                    temp[pos++] = numbers[left++];
+//                else
+//                    temp[pos++] = numbers[mid++];
+//            }
+//            while (left <= eol)
+//                temp[pos++] = numbers[left++];
+//            while (mid <= right)
+//                temp[pos++] = numbers[mid++];
+//            for (i = 0; i < num; i++)
+//            {
+//                numbers[right] = temp[right];
+//                right--;
+//            }
+//        }
+
+//        static public void SortMerge(int[] numbers, int left, int right)
+//        {
+//            int mid;
+//            if (right > left)
+//            {
+//                mid = (right + left) / 2;
+//                SortMerge(numbers, left, mid);
+//                SortMerge(numbers, (mid + 1), right);
+//                MainMerge(numbers, left, (mid + 1), right);
+//            }
+//        }
+
+
+//        static void MainMS(string[] args)
+//        {
+
+
+//            Console.Error.WriteLine("Executing sequential loop...");
+//            Stopwatch stopwatch = new Stopwatch();
+//            stopwatch.Start();
+
+
+//            int[] numbers = { 76, 89, 23, 1, 55, 78, 2, 99, 12, 65 };
+//            Console.WriteLine("MergeSort By Recursive Method");
+//            SortMerge(numbers, 0, numbers.Length-1);
+//            for (int i = 0; i < numbers.Length; i++)
+//                Console.WriteLine(numbers[i]);
+//            //Console.ReadLine();
+
+//            stopwatch.Stop();
+//            Console.Error.WriteLine("Sequential loop time in milliseconds: {0}",
+//                                    stopwatch.ElapsedMilliseconds);
+//        }
+//    }
+//}
---
+++ merge-sort-parallel/MergerSortParalel.cs
@@ -0,0 +1,128 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Diagnostics;
+//using System.Runtime.InteropServices;
+
+
+
+//namespace ParalelComputing
+//{
+//    internal class MergerSortParalel
+//    {
+
+
+//        public int[] arr;
+//        public MergerSortParalel(int[] ARR)
+//        {
+//            this.arr = ARR;
+//        }
+//        static public void merge(int[] arr, int p, int q, int r)
+//        {
+//            int i, j, k;
+//            int n1 = q - p + 1;
+//            int n2 = r - q;
+//            int[] L = new int[n1];
+//            int[] R = new int[n2];
+//            for (i = 0; i < n1; i++)
+//            {
+//                L[i] = arr[p + i];
+//            }
+//            for (j = 0; j < n2; j++)
+//            {
+//                R[j] = arr[q + 1 + j];
+//            }
+//            i = 0;
+//            j = 0;
+//            k = p;
+//            while (i < n1 && j < n2)
+//            {
+//                if (L[i] <= R[j])
+//                {
+//                    arr[k] = L[i];
+//                    i++;
+//                }
+//                else
+//                {
+//                    arr[k] = R[j];
+//                    j++;
+//                }
+//                k++;
+//            }
+//            while (i < n1)
+//            {
+//                arr[k] = L[i];
+//                i++;
+//                k++;
+//            }
+//            while (j < n2)
+//            {
+//                arr[k] = R[j];
+//                j++;
+//                k++;
+//            }
+//        }
+//        static public void mergeSort(int[] arr, int p, int r)
+//        {
+//            if (p < r)
+//            {
+//                int q = (p + r) / 2;
+//                mergeSort(arr, p, q);
+//                mergeSort(arr, q + 1, r);
+//                merge(arr, p, q, r);
+//            }
+//        }
+
+//        static int[] InitializeMatrix(int rows)
+//        {
+//            int[] matrix = new int[rows];
+
+//            Random r = new Random();
+//            for (int i = 0; i < rows; i++)
+//            {
+//                for (int j = 0; j < rows; j++)
+//                {
+//                    matrix[i] = r.Next(100);
+//                }
+//            }
+//            return matrix;
+//        }
+
+
+//        public int[] 
+//        public static void MainMSP()
+//        {
+//            //int[] arr = { 76, 89, 23, 1, 55, 78, 99, 12, 65, 100, 2, 231,123,132,1, 3,4,2,4,6,7,7,7,456,456,34,2 };
+
+//            int rowCount = 2000;
+//            //int[] arr = InitializeMatrix(rowCount);
+
+          
+
+//            int n = 2000, i;
+//            Console.WriteLine("Merge Sort");
+//            Console.Write("Initial array is: ");
+//            for (i = 0; i < n; i++)
+//            {
+//                Console.Write(arr[i] + " ");
+//            }
+//            Console.Error.WriteLine("Executing paralel...");
+//            Stopwatch stopwatch = new Stopwatch();
+//            stopwatch.Start();
+//            mergeSort(arr, 0, n - 1);
+//            Console.Write("\nSorted Array is: ");
+//            for (i = 0; i < n; i++)
+//            {
+//                Console.Write(arr[i] + " ");
+//            }
+//            //Console.ReadLine();
+//            stopwatch.Stop();
+//            Console.WriteLine("Paralel time in milliseconds: {0}",
+//                                    stopwatch.ElapsedMilliseconds);
+//        }
+//    }
+//}
+
+
